a8c76712831505f145e8510f7cddecb37890c1e7
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-10 15:23:10 +0530

    Day-3 Use Case 3 Added

---
+++ CompareTwoLines.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProblems
+{
+    public class CompareTwoLines
+    {
+        public static void display(int x1, int y1, int x2, int y2)
+        {
+
+            {
+                int le1, le2;
+                if (x1 == x2)
+                {
+                    int comp = y1.CompareTo(y2);
+                    if (comp == 0)
+                    {
+                        Console.WriteLine("Both line are equals");
+                    }
+                    else if (comp >= 0)
+                    {
+                        Console.WriteLine("Second line is Smaller than first");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Second line is bigger than First");
+                    }
+                }
+                else
+                {
+                    le1 = y1 - x1;
+                    le2 = y2 - x2;
+                    int comp = le1.CompareTo(le2);
+                    if (comp == 0)
+                    {
+                        Console.WriteLine("Both line are equals");
+                    }
+                    else if (comp >= 0)
+                    {
+                        Console.WriteLine("Second line is Smaller than first");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Second line is bigger than First");
+                    }
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LineComparisionProblems
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            //CompareTwoLines.cs
+
+            //    Console.WriteLine("To Compare Both Line by CompareTo method");
+            //    Console.WriteLine("*****************************************");
+            //    Console.WriteLine("Please Enter the first line coordinates");
+            //    int x1 = Convert.ToInt32(Console.ReadLine());
+            //    int y1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Enter start and end points of Second line:");
+            //    int x2 = Convert.ToInt32(Console.ReadLine());
+            //    int y2 = Convert.ToInt32(Console.ReadLine());
+            //    CompareTwoLines.display(x1, y1, x2, y2);
+
+
+            //EquOfTwoLine.cs
+
+            // EquOfTwoLine.Check();
+
+
+            //CartesianSystem.cs
+            CartesianSystem.main();
+        }
+    }
+}
